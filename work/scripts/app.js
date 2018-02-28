5623388ded80261731bca5a6cbfd5c0460c1a599
Debasish Mohanty <debasishm@mindfiresolutions.com>
2018-02-28 11:50:09 +0530

    made changes in the js fiel in branch2

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
+//see this is the js script where i have added some comments
 (function() {
   'use strict';
 
